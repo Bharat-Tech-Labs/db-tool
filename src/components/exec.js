456693bbba80b3aa76aa83981fe2a1966a1382c1
brijesh-k-bharattechlabs-com <67728046+brijesh-k-bharattechlabs-com@users.noreply.github.com>
2020-08-05 10:45:19 +0530

    Delete exec.js

--- src/components/exec.js
+++
@@ -1,9 +0,0 @@
-const exec = require('child_process').exec;
-exec('node try.js',function(err,stdout,stderr){
-                console.log(stdout);
-            });
-
-
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            // const shell = require('shelljs');
-            // shell.exec('node -v');
